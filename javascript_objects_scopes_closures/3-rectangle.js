ae0fb6589efbc6bf56febe683ad7b1bf8323c5d3
Kurapika <victorakhihiero@gmail.com>
2023-09-04 21:29:15 +0100

    check

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,13 +9,14 @@ class Rectangle {
   }
 
   print() {
-    for(let i = 0; i < this.height; i++){
-        let row ="";
-        for(let j =0; j< this.width; j++){
-            row += "x"
-        }
-        console.log(row)
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'x';
+      }
+      console.log(row);
     }
   }
 }
 
+module.exports = Rectangle;
