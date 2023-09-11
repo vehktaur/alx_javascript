24f76d65e7a97e64facaff75d5861e909f9fd9b2
Kurapika <victorakhihiero@gmail.com>
2023-09-11 21:22:33 +0100

    check

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,27 @@
+//#!usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+let todo = [];
+let myObject = {};
+let value = 1;
+
+request.get(url, (error, response, body) => {
+  todo = JSON.parse(body);
+  for (let obj of todo) {
+    if (obj.completed) {
+      let property = obj.userId;
+      if (myObject.hasOwnProperty(property)) {
+        value = value;
+      } else {
+        value = 1;
+      }
+      myObject = {
+        ...myObject,
+        [property]: obj.completed ? value++ : value
+      };
+    }
+  }
+  console.log(myObject);
+});
