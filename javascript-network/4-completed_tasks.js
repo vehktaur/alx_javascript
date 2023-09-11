07514479c1c5cfa14de419d4a4d619bf9c4d5f0e
Kurapika <victorakhihiero@gmail.com>
2023-09-11 21:47:15 +0100

    check

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -5,21 +5,20 @@ const url = process.argv[2];
 
 let todo = [];
 let myObject = {};
-let value = 1;
+let value;
 request.get(url, (error, response, body) => {
   todo = JSON.parse(body);
   for (let obj of todo) {
+    value = 0
     if (obj.completed) {
       let property = obj.userId;
       if (myObject.hasOwnProperty(property)) {
         property = String(property);
-        let value = Number(myObject[property]);
-      } else {
-        value = 1;
+        value = Number(myObject[property]);
       }
       myObject = {
         ...myObject,
-        [property]: obj.completed ? value++ : value
+        [property]: value+1
       };
     }
   }
